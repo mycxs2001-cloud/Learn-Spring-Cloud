c9d3db318d4a2dc40dd66e8ebe53327f9238b73b
my_cxs <1692184814@qq.com>
2025-09-09 13:35:36 +0800

    将service-passenger-user  注册到nacos

--- online-taxi-public/service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/ServicePassengerUserApplication.java
+++ online-taxi-public/service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/ServicePassengerUserApplication.java
@@ -3,6 +3,7 @@ package com.mashibing.servicepassengeruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @version: java version 1.8
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.mashibing.servicepassengeruser.mapper")
+@EnableDiscoveryClient
 public class ServicePassengerUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServicePassengerUserApplication.class);
